71077daf845001c570cee48bffd1eb213a6cf8a0
Maysara Safwat <maisarasafwat2@gmail.com>
2023-05-08 21:39:51 +0300

    rm

--- server/config/DBconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-// DATABASE CONFIG
-define("__HOST__", "scandiweb-test-task-db.cjevyoxhqmo6.us-east-1.rds.amazonaws.com");
-define("__PORT__", "3306");
-define("__USER__", "maysara");
-define("__PASS__", "maysarasafwat");
-define("__DB__", "scandiweb_test");
-//define("__Debug__Mode__", 1);
